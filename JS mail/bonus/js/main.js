cfdbefc90b257170f931a031c417a6f6c0377244
CarloSalaris <carloa.salaris@gmail.com>
2023-05-03 19:02:19 +0200

    JS Mail bonus input + button

---
+++ JS mail/bonus/js/main.js
@@ -0,0 +1,51 @@
+/**************************
+    JS MAIL - BONUS
+ **************************/
+
+//Creare un array di email con diritto di accesso
+const mailArray = ["shannon08@eichmann.com",
+    "samson04@lind.com",
+    "cthompson@hotmail.com",
+    "kling.ruthe@halvorson.org",
+    "lehner.jadyn@williamson.com",
+    "arely57@yahoo.com",
+    "carloa.salaris@gmail.com",
+    "sister_08@hotmail.com",
+    "schaden.mario@pagac.com",
+    "brody.doyle@yahoo.com",
+    "keebler.kathleen@jakubowski.com",];
+const inputConfirm = document.getElementById("inputConfirm");
+
+let registered = false;
+
+inputConfirm.addEventListener("click", 
+
+    function() {
+        //Chiedi all’utente la sua email
+        const userMail = document.getElementById("userMail").value;
+
+        //controlla che sia nella lista di chi può accedere
+        //scorriamo l'array (con un for loop) + stampa un messaggio appropriato sull’esito del controllo.
+        for (let i = 0; i < mailArray.length; i++) {
+            
+            //Se il valore di userMail corrisponde a un valore contenuto nell'array: accesso consentito
+            if (userMail == mailArray[i]) {
+                registered = true;   
+            }
+        }
+    
+        //Output risultato
+        if (registered === true) {
+            alert("Accesso consentito");
+            console.log("Accesso consentito");
+        } else {
+            alert("Accesso negato");
+            console.log("Accesso negato");
+        }
+
+        console.log(registered);
+    }
+
+);
+
+
