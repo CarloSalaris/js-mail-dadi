c0aa20640f10863b0f10a79a6045d7430645868c
CarloSalaris <carloa.salaris@gmail.com>
2023-05-03 15:20:51 +0200

    JS - Mail commenti struttura logica

---
+++ js/mail.js
@@ -0,0 +1,9 @@
+//Chiedi all’utente la sua email
+    //inserimento userMail tramite prompt
+
+//controlla che sia nella lista di chi può accedere
+    //Creare un array di email con diritto di accesso
+    //scorriamo l'array (con un for loop) + stampa un messaggio appropriato sull’esito del controllo.   
+        //Se il valore di userMail corrisponde a un valore contenuto nell'array: accesso consentito
+        //Se non corrisponde a nessun valore: accesso negato
+
