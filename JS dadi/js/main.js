a85fd6a9f02556590e7bd750f9724e6f03c8bc18
CarloSalaris <carloa.salaris@gmail.com>
2023-05-03 17:25:06 +0200

    JS Dadi - generato punteggi

--- JS dadi/js/main.js
+++ JS dadi/js/main.js
@@ -4,8 +4,11 @@
 
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
     // creare punteggio giocatore (playerPoints)
+const playerPoints = Math.ceil(Math.random() * 6);
     // creare punteggio computer (ComputerPoints)
+const ComputerPoints = Math.ceil(Math.random() * 6);
 
+console.log(playerPoints, ComputerPoints);
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
     // Se il punteggio giocatore è maggiore del punteggio computer: "HAI VINTO!"
     // Se il punteggio giocatore è uguale al punteggio computer:"PARI"
