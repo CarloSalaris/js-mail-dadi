1dd3c38132ea42e0b3bcb76c08f199e330b2ed51
CarloSalaris <carloa.salaris@gmail.com>
2023-05-03 17:15:51 +0200

    JS Dadi - Commenti processo

---
+++ JS dadi/js/main.js
@@ -0,0 +1,12 @@
+/**************************
+    Gioco dei dadi
+ **************************/
+
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    // creare punteggio giocatore (playerPoints)
+    // creare punteggio computer (ComputerPoints)
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    // Se il punteggio giocatore è maggiore del punteggio computer: "HAI VINTO!"
+    // Se il punteggio giocatore è uguale al punteggio computer:"PARI"
+    // Altrimenti: "HA VINTO IL COMPUTER!"
