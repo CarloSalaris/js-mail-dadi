2c535520df739ca77ced9752540a20a2a25b18ab
CarloSalaris <carloa.salaris@gmail.com>
2023-05-03 17:46:23 +0200

    JS Dadi - working with button click

---
+++ JS dadi/bonus/js/main.js
@@ -0,0 +1,37 @@
+/**************************
+    Gioco dei dadi
+ **************************/
+
+const playButton = document.getElementById("play");
+
+playButton.addEventListener("click",
+
+    function () {
+        // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    // creare punteggio giocatore (playerPoints)
+    const playerPoints = Math.ceil(Math.random() * 6);
+    // creare punteggio computer (ComputerPoints)
+const ComputerPoints = Math.ceil(Math.random() * 6);
+
+console.log(playerPoints, ComputerPoints);
+
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+    // Se il punteggio giocatore è maggiore del punteggio computer: "HAI VINTO!"
+    if (playerPoints > ComputerPoints) {
+        
+        alert("HAI VINTO!");
+    // Se il punteggio giocatore è uguale al punteggio computer:"PARI"
+    } else if (playerPoints === ComputerPoints) {
+        
+        alert("PAREGGIO");
+    // Altrimenti: "HA VINTO IL COMPUTER!"
+    } else {
+        
+        alert("HAI PERSO");
+
+    }
+    }
+
+);
+
+    
